0c8204deb7223855c93da6708273436d25bd8597
iamceltic33 <iamceltic9@gmail.com>
2018-10-23 22:08:07 +0600

    db

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const cors = require('cors');
 
+const { Pool } = require('pg');
+let e = '';
+const pool = new Pool({
+    user: 'postgres',
+    database: 'postgres',
+    password: 'password'
+});
+
+pool.query('SELECT * FROM parkings', (err, res) => {
+    if (err) throw err;
+
+    pool.end()
+  })
+
+
+
 var app = express()
 app.use(cors())
 const data = {
@@ -22,7 +38,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.end('Server is running'))
+  .get('/', (req, res) => res.end('Server is running addons'))
   .get('/test', (req, res) => {res.end(JSON.stringify(data))})
   .get('pg', (req, res)=> {  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
